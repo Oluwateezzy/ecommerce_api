11d3c591900d0d7d42e0711a21cc6209ae088aaa
oluwateezzy <oluwateezzy03@gmail.com>
2022-08-31 12:33:15 +0100

    working on user

--- app.js
+++ app.js
@@ -1,1 +1,65 @@
-console.log('E-Commerce API');
+require("dotenv").config();
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+// utils Packages
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+const fileUpload = require("express-fileupload");
+const rateLimiter = require("express-rate-limit");
+const helmet = require("helmet");
+const xss = require("xss-clean");
+const cors = require("cors");
+const mongoSanitize = require("express-mongo-sanitize");
+
+// database
+const connectDB = require("./db/connect");
+
+//router
+const userRouter = require("./routes/userRoutes");
+
+// middleware
+const notFoundMiddleware = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
+app.set("trust proxy", 1);
+app.use(
+    rateLimiter({
+        windowMs: 15 * 60 * 1000,
+        max: 60,
+    })
+);
+
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+app.use(mongoSanitize());
+
+app.use(express.json());
+app.use(cookieParser(process.env.JWT_SECRET));
+
+app.use(express.static("./public"));
+app.use(fileUpload());
+
+// routes
+//app.use("/api/v1/users", userRouter);
+
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
+
+const port = process.env.PORT || 3000;
+
+const start = async() => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => {
+            console.log(`Server is listening on port ${port}...`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+start();
